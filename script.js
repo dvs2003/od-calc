0995ca82ce5e5fb0458bd1ec538820feceb0c6fd
dvs2003 <50419946+dvs2003@users.noreply.github.com>
2024-08-16 16:31:07 +0530

    UI complete and input being taken

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const allKeys = {
-    0: ['N/A', 'modifier'],
+    0: ['N/A', 'currentOp'],
     1 : ['+-', 'modifier'],
     2 : ['%', 'modifier'],
     3 : ['AC', 'reset'],
-    4 : [7, 'number'],
-    5 : [8, 'number'],
-    6 : [9, 'number'],
+    4 : [7, 'num'],
+    5 : [8, 'num'],
+    6 : [9, 'num'],
     7 : ['div', 'operator'],
     8 : [4, 'num'],
     9 : [5, 'num'],
@@ -42,11 +42,30 @@ function createCalculator(parentDiv, numberRows, numberCols) {
             
             const newSquare = document.createElement("div");
             newSquare.classList.add("eachSquare");
+            
+            if (indexRow == 0 && indexCol == 0) {
+                newSquare.classList.add('currentOp');
+            }
+
+            if (allKeys[Object.keys(allKeys)[indexCol + (indexRow * 4)]][1] == 'num') {
+                newSquare.classList.add('normalKey');
+            } else if (allKeys[Object.keys(allKeys)[indexCol + (indexRow * 4)]][1] == 'execute') {
+                newSquare.classList.add('exeKey');
+            } else if (indexRow != 0 || indexCol != 0) {
+                newSquare.classList.add('lightKey');
+            }
 
             const keyText = document.createElement('span');
             keyText.textContent = allKeys[Object.keys(allKeys)[indexCol + (indexRow * 4)]][0];
             keyText.classList.add('keyText');
+            
             newSquare.appendChild(keyText);
+
+            newSquare.addEventListener("click", (event) => {
+                const btn = event.target;
+                console.log(btn.textContent);
+            });
+
             newRow.appendChild(newSquare);
             
         }
